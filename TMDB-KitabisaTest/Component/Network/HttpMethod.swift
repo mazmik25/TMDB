bf53ce3386c2513cdf375dbd4ac2563e5fd9b207
mazmik25 <m.azmi65@yahoo.com>
2020-02-22 20:58:43 +0700

    adding HttpMethod file

---
+++ TMDB-KitabisaTest/Component/Network/HttpMethod.swift
@@ -0,0 +1,20 @@
+//
+//  HttpMethod.swift
+//  TMDB-KitabisaTest
+//
+//  Created by Azmi Muhammad on 22/02/20.
+//  Copyright © 2020 Azmi Muhammad. All rights reserved.
+//
+
+import Foundation
+enum HttpMethod: String {
+    case connect = "CONNECT"
+    case delete = "DELETE"
+    case get = "GET"
+    case head = "HEAD"
+    case options = "OPTIONS"
+    case patch = "PATCH"
+    case post = "POST"
+    case put = "PUT"
+    case trace = "TRACE"
+}
